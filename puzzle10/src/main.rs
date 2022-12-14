d15d9fa7c54675ebd8da71ac921fe6131bcee603
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2022-12-14 10:37:48 +0100

    feat: initial setup

---
+++ puzzle10/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+fn get_input_content() -> String {
+    let file_path = "./puzzle10/input.txt";
+    let contents = fs::read_to_string(file_path).expect("Failed to read the file");
+    contents
+}
+
+fn main() {
+    let content = get_input_content();
+
+    for line in content.lines() {
+        println!("{line}");
+    }
+}
